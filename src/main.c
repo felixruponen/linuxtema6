9ed03dcd24aaaa4e9bac72b0623f1e28fb8dd6b5
thedeukalion <deukalion@live.se>
2014-11-24 14:09:01 +0100

    Komplett inmatning och formatering

--- src/main.c
+++ src/main.c
@@ -29,8 +29,31 @@ int main()
 		i++;
 	}
 	float resultRes = calc_resistance(componentCount,conn,components);
-	printf("Ers�ttningsresistans:%f\n",resultRes);
+	printf("Ersättningsresistans: %.0f Ohm\n", resultRes);
+
 	float power = calc_power_r (current,resultRes);
-	printf("Effektutveckling:%f\n",power);
+	printf("Effekt: %.2f W\n",power);
+
+	int resultECount = e_resistance(resultRes, components);
+
+	printf("Ersättningsresistans i E12-serien kopplade i serier: ");
+
+	i=0;
+	int i2 = 0;
+
+	while(i < componentCount && i2 < resultECount)
+	{
+
+		if (components[i] != 0)
+		{
+			printf( ((i2 == (resultECount-1)) ? "%.0f" : "%.0f, "), components[i]);
+			i2++;
+		}
+
+		i++;
+	}
+
+	printf("\n");
+
 	return 0;
 }
